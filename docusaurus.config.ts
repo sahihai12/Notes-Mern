9d239725ef617d433cf74fa65c30f737643c4e13
sahihai12 <skp28195@gmail.com>
2024-11-09 02:10:16 +0530

    ['ui' , home page ui changes]

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,30 +2,19 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
-  title: 'My Site',
+  title: 'Tech Stack',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'https://github.com/sahihai12', 
+  projectName: 'tech-stack',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -37,10 +26,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -48,11 +33,6 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
@@ -65,12 +45,9 @@ const config: Config = {
   ],
 
   themeConfig: {
-    docs: {
-      editUrl: null, 
-    },
     colorMode: {
       defaultMode: 'dark',
-      respectPrefersColorScheme: true, // Optional: respect user's system color scheme preference
+      respectPrefersColorScheme: true, 
     },
     navbar: {
       title: 'Tech Stack',
@@ -85,7 +62,6 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/sahihai12',
           label: 'GitHub',
@@ -95,7 +71,7 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()}`,
     },
     prism: {
       theme: prismThemes.github,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,26 +2,58 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
+import bgImage from '../../static/img/image-bg.png';
+import leetcode from '../../static/img/leetcode.png';
+import github from '../../static/img/github.png';
+import codepen from '../../static/img/codepen.png';
+import linkedIn from '../../static/img/linkedin.png';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero ', styles.heroBanner)}>
+      
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+        <div className="row">
+          <div className="col-md-6">
+          <div className="wrap-align">
+            <Heading as="h1" className="hero__title">
+              Hi! I<span className='p-color'>'m</span> Sahil
+            </Heading>
+            <Heading as="h2" className="hero__sub__title">
+              
+            </Heading>
+            <p className="hero__subtitle">
+              Results-driven Software Engineer with 4+ years of experience specializing in the <b className='p-color'>MERN</b> stack (MongoDB, Express.js, React, Node.js). Proven track record in designing, developing, and deploying scalable web applications across eCommerce, learning management, and trip planning domains. Adept at leading frontend teams, building RESTful APIs, and optimizing performance. Skilled in handling full-stack development, database management, and cloud infrastructure (AWS).
+            </p>
+            <ul>
+              <li><Link
+                className=""
+                to="https://github.com/sahihai12">
+                <img src={github} alt="" />
+              </Link></li>
+              <li><Link
+                className=""
+                to="https://www.linkedin.com/in/sahil-prajapati-7b8176177/">
+                <img src={linkedIn} alt="" />
+              </Link></li>
+              <li><Link
+                className=""
+                to="https://leetcode.com/u/Sahil201201/">
+                <img src={leetcode} alt="" />
+              </Link></li>
+              <li><Link
+                className=""
+                to="https://codepen.io/sahihai12">
+                <img src={codepen} alt="" />
+              </Link></li>
+            </ul>
+          </div>
+          </div>
+          <div className="col-md-6">
+            <img src={bgImage} alt="" className='img-fluid'/>
+          </div>
         </div>
       </div>
     </header>
@@ -34,9 +66,8 @@ export default function Home(): JSX.Element {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <HomepageHeader />
       </main>
     </Layout>
   );
